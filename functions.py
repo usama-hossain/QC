351c82d16e0d5a7429278fb8dadfa04339b43f3c
Usama Hossain <mohammed.usama@northsouth.edu>
2021-01-29 22:26:39 +0600

    isSquare and isHermitian functions added

---
+++ functions.py
@@ -0,0 +1,14 @@
+# functions from programming drills exercises
+
+import numpy as np
+
+def isSquare(A):
+    rows = len(A)
+    for row in A:
+        if len(row) != rows
+            return False
+    return True
+
+def isHermitian(A):
+    if isSquare(A) == False:
+        return False
